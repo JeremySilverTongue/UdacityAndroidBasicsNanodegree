da389707eac5529e550baa4aa5e26d1178b60a46
Jeremy Silver <jeremy@udacity.com>
2016-06-03 13:41:53 -0700

    Add initial version of CourtCounter

---
+++ 2-CourtCounter/app/src/main/java/com/udacity/silver/courtcounter/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.udacity.silver.courtcounter.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.udacity.silver.courtcounter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
